37298d2dd828e08c9e69faa3957f2a8bb2070f88
Arturo Cuya <arturocuya099@gmail.com>
2019-03-28 12:25:35 -0500

    Ahora el programa cuenta vocales

--- program.py
+++ program.py
@@ -1,12 +1,14 @@
 palabra = input("Ingrese una palabra: ")
 
 letras = {}
+vocales = ['a','e','i','o','u']
 
 for letra in palabra:
-	if letra not in letras.keys():
-		letras[letra] = 1
-	else:
-		letras[letra] += 1
+	if letra in vocales:
+		if letra not in letras.keys():
+			letras[letra] = 1
+		else:
+			letras[letra] += 1
 
 for letra in letras.keys():
 	print(letra + ":", letras[letra])
